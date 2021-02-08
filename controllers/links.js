95b2a22c2916487b1cd9db951d6d8a32e2a48073
Konstantinos <kostas.droid@gmail.com>
2021-02-08 18:25:03 +0100

    added error handler and asynchandler

--- controllers/links.js
+++ controllers/links.js
@@ -1,62 +1,53 @@
 const Link = require("../models/Link");
-
+const ErrorResponse = require('../utils/errorResponse');
+const asyncHandler = require('../middleware/async');
 
 // @desc    Get all link
 // @route   GET /
 // @acess   Private/Admin
-exports.getlinks = async (req, res, next) => {
-    try {
-         const link = await Link.find();
+exports.getlinks = asyncHandler(async (req, res, next) => {
+    
+    const link = await Link.find();
  
-         
-         res.status(200).json({sucess: true, count:link.length,  data: link}); 
-     } catch (err) {
-         res.status(400).json({success: false});
-     }
-         
- }
+    res.status(200).json({sucess: true, count:link.length,  data: link}); 
+             
+ });
 
 
 // @desc    Get single link
 // @route   GET /:id
 // @acess   Public
-exports.getlink = async (req, res, next) => {
-   try {
+exports.getlink = asyncHandler(async (req, res, next) => {
+   
         const link = await Link.findOne({url : req.params.id});
 
         if (!link) {
-            return res.status(400).json({sucess: false});
+            return next(new ErrorResponse(`Url not found with name of ${req.params.id}`, 404));
+
         }
 
         res.status(200).json({sucess: true, data: link}); 
-    } catch (err) {
-        res.status(400).json({success: false});
-    }
         
-}
+});
 
 // @desc    Create new link
 // @route   POST /
 // @acess   Public
-exports.createlink = async(req, res, next) => {
-    try{
-        const link = await Link.create(req.body);
-    //console.log(req.body);
+exports.createlink = asyncHandler(async(req, res, next) => {
     
+    const link = await Link.create(req.body);
+        
     res.status(201).json({
         sucess: true,
         data: link
     }); 
-} catch(err) {
-    res.status(400).json({sucess:false });
-}    
-
-};
+ 
+});
 
 // @desc    Update link
 // @route   PUT /:id
 // @acess   Private
-exports.updatelink = async (req, res, next) => {
+exports.updatelink = asyncHandler(async (req, res, next) => {
     const link = await Link.findOneAndUpdate({url : req.params.id}, req.body, {
         new: true,
         runValidators: true
@@ -67,13 +58,13 @@ exports.updatelink = async (req, res, next) => {
     }
 
     res.status(200).json({sucess: true, data: link});     
-}
+});
 
 
 // @desc    Delete link
 // @route   PELETE /:id
 // @acess   Private
-exports.deletelink = async (req, res,  next) => {
+exports.deletelink = asyncHandler(async (req, res,  next) => {
     const link = await Link.findOneAndDelete({url : req.params.id});
 
     if(!link){
@@ -81,4 +72,4 @@ exports.deletelink = async (req, res,  next) => {
     }
 
     res.status(200).json({sucess: true, data: {} });
-}    
+});    
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 const morgan = require('morgan');
 const connectDB = require('./config/db');
 const colors = require('colors');
+const errorHandler = require('./middleware/error');
 
 
 // Load env vars
@@ -28,6 +29,8 @@ if(process.env.NODE_ENV === 'development') {
 // Mount routers
 app.use('/', links);
 
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
