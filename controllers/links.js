24de0a7500f57b0df8a8ec5f7d0c409522b191f9
Konstantinos <kostas.droid@gmail.com>
2021-02-09 12:17:03 +0100

    Admin can delete and update unregistered links

--- controllers/links.js
+++ controllers/links.js
@@ -84,10 +84,15 @@ exports.updatelink = asyncHandler(async (req, res, next) => {
     if(!link){
         return next(new ErrorResponse(`Url not found with name of ${req.params.id}`, 404));
 
-    }   
+    }
+    
+    if(req.user.role == "admin"){
+        link.remove();
+        return res.status(200).json({sucess: true, data: link});  
+    }
 
     //Make sure user is link owner
-    if(link.user.toString() !== req.user.id) {
+    if(link.user.toString() !== req.user.id && req.user.role !== "admin") {
         return next(new ErrorResponse(`User ${req.user.id} is not authorized to update this link`, 401));
     }
 
@@ -110,8 +115,10 @@ exports.deletelink = asyncHandler(async (req, res,  next) => {
         return next(new ErrorResponse(`Url not found with name of ${req.params.id}`, 404));
     }
 
+    
+
     //Make sure user is link owner
-    if(link.user.toString() !== req.user.id) {
+    if(link.user.toString() !== req.user.id && req.user.role !== "admin") {
         return next(new ErrorResponse(`User ${req.user.id} is not authorized to delete this link`, 401));
     }
 
