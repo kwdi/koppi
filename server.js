241c7308ff4d5696b12f7f1300b31e16f3504bbc
Konstantinos <kostas.droid@gmail.com>
2021-02-09 12:39:04 +0100

    api security

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const connectDB = require('./config/db');
 const colors = require('colors');
 const errorHandler = require('./middleware/error');
 const cookieParser = require('cookie-parser');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
 
 // Load env vars
 dotenv.config({path: './config/config.env'});
@@ -29,6 +35,28 @@ app.use(cookieParser());
 if(process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
+
+// Sanitize data
+app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 mins
+  max: 10
+});
+app.use(limiter);
+
+// Prevent http param pollution
+app.use(hpp());
+
+// Enable CORS
+app.use(cors());
 
 // Mount routers
 app.use('/', links);
